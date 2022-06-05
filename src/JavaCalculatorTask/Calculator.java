de461c778ae6e52acf3fa2102ed1fe67eedc5bbb
kimho <rlagh1073@naver.com>
2022-06-05 13:02:22 +0900

    java gui 계산기 1차 완성

--- src/JavaCalculatorTask/Calculator.java
+++ src/JavaCalculatorTask/Calculator.java
@@ -7,7 +7,11 @@ public class Calculator extends JFrame {
     private double op1=0;
     private double op2=0;
     private String operator;
-    private JButton[] btnNum= new JButton[10];
+    private String[] btnNum= {
+            "7","8","9",
+            "4","5","6",
+            "1","2","3","0"
+};
 
     public Calculator(){ // 생성자
         JLabel status=new JLabel("버튼을 클릭하세요");
@@ -20,53 +24,127 @@ public class Calculator extends JFrame {
         display.setHorizontalAlignment(JTextField.RIGHT);
         this.add(BorderLayout.NORTH,display);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4,4));
+        panel.setLayout(new GridLayout(5,4));
         JButton btnPlus = new JButton("+");
+        btnPlus.setFont(new Font("맑은 고딕",Font.BOLD,15));
         JButton btnMinus = new JButton("-");
-        JButton btnMulti = new JButton("*");
+        btnMinus.setFont(new Font("맑은 고딕",Font.BOLD,20));
+        JButton btnMulti = new JButton("X");
+        btnMulti.setFont(new Font("맑은 고딕",Font.BOLD,15));
         JButton btnDivision = new JButton("/");
+        btnDivision.setFont(new Font("맑은 고딕",Font.BOLD,15));
         JButton btnDot=new JButton(".");
+        btnDot.setFont(new Font("맑은 고딕",Font.BOLD,15));
         JButton btnEqual = new JButton("=");
-        btnNum[0]=new JButton("7");
-        btnNum[1]=new JButton("8");
-        btnNum[2]=new JButton("9");
-        btnNum[3]=new JButton("4");
-        btnNum[4]=new JButton("5");
-        btnNum[5]=new JButton("6");
-        btnNum[6]=new JButton("1");
-        btnNum[7]=new JButton("2");
-        btnNum[8]=new JButton("3");
-        btnNum[9]=new JButton("0");
-        panel.add(btnNum[0]);
-        panel.add(btnNum[1]);
-        panel.add(btnNum[2]);
-        panel.add(btnPlus);
-        panel.add(btnNum[3]);
-        panel.add(btnNum[4]);
-        panel.add(btnNum[5]);
-        panel.add(btnMinus);
-        panel.add(btnNum[6]);
-        panel.add(btnNum[7]);
-        panel.add(btnNum[8]);
-        panel.add(btnMulti);
-        panel.add(btnDot);
-        panel.add(btnNum[9]);
-        panel.add(btnEqual);
-        panel.add(btnDivision);
+        btnEqual.setFont(new Font("맑은 고딕",Font.BOLD,15));
+
         for(int i=0;i<10;i++){
-            btnNum[i].addActionListener(e->{
-                if(display.getText().equals("0")){
-                    // display.setText(btnNum.getText());
+            JButton btn = new JButton(btnNum[i]);
+            btn.setFont(new Font("맑은 고딕",Font.BOLD,15));
+            panel.add(btn);
+            if(i==2){
+                panel.add(btnPlus);
+            }
+            if(i==5){
+                panel.add(btnMinus);
+            }
+            if(i==8){
+                panel.add(btnMulti);
+            }
+            if(i==8){
+                panel.add(btnDivision);
+            }
+            if(i==9) {
+                panel.add(btnDot);
+                panel.add(btnEqual);
+            }
+            btn.addActionListener(e->{
+                if(display.getText().equals("0")){ //참조연산 비교는 equals 사용
+                    display.setText(btn.getText());
+                }else {
+                    display.setText(display.getText() + btn.getText());
                 }
             });
+
         }
 
+        btnEqual.addActionListener(e->{ // = 클릭이벤트
 
+            op2=Double.parseDouble(display.getText());
 
+            double result=calc(op1,op2,operator);
+            display.setText(""+result);
+            status.setText(""+result);
+
+        });
+
+        btnPlus.addActionListener(e->{ // + 클릭이벤트
+            op1=Double.parseDouble(display.getText());
+
+            display.setText("0");
+            operator="+";
+            status.setText(op1+"+");
+        });
+        btnMinus.addActionListener(e->{ // - 클릭이벤트
+            op1=Double.parseDouble(display.getText());
+            display.setText("0");
+            operator="-";
+            status.setText(op1+"-");
+        });
+        btnDivision.addActionListener(e->{ // / 클릭이벤트
+            op1=Double.parseDouble(display.getText());
+            display.setText("0");
+            operator="/";
+            status.setText(op1+"/");
+        });
+        btnMulti.addActionListener(e->{ // X 클릭이벤트
+            op1=Double.parseDouble(display.getText());
+            display.setText("0");
+            operator="X";
+            status.setText(op1+"X");
+        });
+        btnDot.addActionListener(e->{ // . 클릭이벤트
+            if(!display.getText().contains(".")){
+                display.setText(display.getText()+btnDot.getText());
+            }
+        });
+
+        JButton btnClear = new JButton("C");
+        btnClear.addActionListener(e->{ // C 클릭이벤트
+            op1=0;
+            op2=0;
+            operator="+";
+            display.setText("0");
+            status.setText(" ");
+        });
+        panel.add(btnClear);
+
+        JButton btnBackspace = new JButton("<-");
+        btnBackspace.addActionListener(e->{ // <- 클릭이벤트
+            String temp = display.getText();
+            temp=temp.substring(0,temp.length()-1);
+            display.setText(temp);
+        });
+        panel.add(btnBackspace);
 
         this.add(BorderLayout.CENTER,panel);
 
         this.add(BorderLayout.SOUTH,status);
+    }
+
+    private double calc(double op1, double op2, String operator) {
+        switch (operator){
+            case "+":
+                return op1+op2;
+            case "-":
+                return op1-op2;
+            case "X":
+                return op1*op2;
+            case "/":
+                return op1/op2;
+            default:
+                return 0;
+        }
     }
 
     public static void main(String[] args) {
